437eb7e20f081ce8fd534f24caa595f8e27ae5cb
Nicolas Figueredo <gtyt@outlook.com.ar>
2026-06-22 23:25:55 -0300

    ss

--- public/build/assets/CardTitle.vue_vue_type_script_setup_true_lang-DwchAzwz.js
+++
@@ -1,6 +0,0 @@
-import{c as d,a as n}from"./Button.vue_vue_type_script_setup_true_lang-BSZnszBV.js";import{d as t,f as c,b as o,s as l,n as r,u as p}from"./app-9ME6nIAM.js";/**
- * @license lucide-vue-next v0.468.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const f=d("PencilIcon",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]]),_=t({__name:"CardHeader",props:{class:{}},setup(s){const a=s;return(e,i)=>(o(),c("div",{class:r(p(n)("flex flex-col gap-y-1.5 p-6",a.class))},[l(e.$slots,"default")],2))}}),h=t({__name:"CardTitle",props:{class:{}},setup(s){const a=s;return(e,i)=>(o(),c("h3",{class:r(p(n)("text-2xl font-semibold leading-none tracking-tight",a.class))},[l(e.$slots,"default")],2))}});export{f as P,_,h as a};
